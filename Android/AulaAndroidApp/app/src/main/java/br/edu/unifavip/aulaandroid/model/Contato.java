c36af023c7c406dda7e058a78036c5b4483bcf3e
Wolney Freitas <wolneyhqf@gmail.com>
2019-11-28 13:43:22 -0300

    Aula de Android

---
+++ Android/AulaAndroidApp/app/src/main/java/br/edu/unifavip/aulaandroid/model/Contato.java
@@ -0,0 +1,50 @@
+package br.edu.unifavip.aulaandroid.model;
+
+public class Contato {
+
+    private Integer id;
+    private String nome;
+    private String telefone;
+    private String email;
+
+    public Contato(){
+
+    }
+
+    public Contato(String nome, String telefone){
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
